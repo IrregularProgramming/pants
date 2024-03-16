950136721ac597a2522e0381dbc26d6b1af44777
Fredrik Sjöborg <fredrik.sjoborg@twoday.com>
2024-03-16 15:47:52 +0100

    image folder

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import { ImageBackground } from "react-native";
+
 import {
   SafeAreaView,
   ScrollView,
@@ -63,38 +65,14 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next: test
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ImageBackground
+      style={{width: '100%', height: '100%'}}
+      source={require('./android/app/images/bgpants.png')}>
+      <Text>test2</Text>
+    </ImageBackground>
   );
 }
+
 
 const styles = StyleSheet.create({
   sectionContainer: {
